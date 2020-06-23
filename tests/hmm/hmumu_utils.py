0b634a0d34d3c2d0b8d00e0d5da25d091afeeafb
Irene Dutta <irene.dutta@cern.ch>
2020-06-23 16:26:33 -0700

    fsr or geofit condition

--- tests/hmm/hmumu_utils.py
+++ tests/hmm/hmumu_utils.py
@@ -31,7 +31,7 @@ NUMPY_LIB = None
 debug = False
 #debug = True
 #event IDs for which to print out detailed information
-debug_event_ids = [1464046016]
+debug_event_ids = [178677823]
 #Run additional checks on the analyzed data to ensure consistency - for debugging
 doverify = False
 
